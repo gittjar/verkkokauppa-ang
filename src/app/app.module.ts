ca8d56e308aed2f3aa02e31285ca17cc746fe8af
gittjar <jarno.keranen@gmail.com>
2025-03-24 19:11:40 +0200

    firebase deploy and dist/

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-// importsit! ng add @angular/material
-
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatIconModule} from '@angular/material/icon';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatListModule} from '@angular/material/list';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatTableModule} from '@angular/material/table';
-import {MatBadgeModule} from '@angular/material/badge';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {MatChipsModule} from '@angular/material/chips';
-
-
-    
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { NgSwitch } from '@angular/common';
+
+// Angular Material Modules
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatChipsModule } from '@angular/material/chips';
+
+// Components
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ProductsHeaderComponent } from './pages/home/components/products-header/products-header.component';
 import { FiltersComponent } from './pages/home/components/filters/filters.component';
 import { ProductBoxComponent } from './pages/home/components/product-box/product-box.component';
 import { CartComponent } from './pages/cart/cart.component';
+import { MaterialBoxComponent } from './pages/home/components/material-box/material-box.component';
+
+// Services
 import { CartService } from './services/cart.service';
 import { StoreService } from './services/store.service';
 import { MaterialstoreService } from './services/materialstore.service';
-import { HttpClientModule } from '@angular/common/http';
-import { MaterialBoxComponent } from './pages/home/components/material-box/material-box.component';
-import { NgSwitch } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -50,7 +50,9 @@ import { NgSwitch } from '@angular/common';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    // Modules kauppaa varten alla!
+    HttpClientModule,
+    NgSwitch,
+    // Angular Material Modules
     MatSidenavModule,
     MatGridListModule,
     MatMenuModule,
@@ -63,9 +65,7 @@ import { NgSwitch } from '@angular/common';
     MatTableModule,
     MatBadgeModule,
     MatSnackBarModule,
-    MatChipsModule,
-    HttpClientModule,
-    NgSwitch
+    MatChipsModule
   ],
   providers: [CartService, StoreService, MaterialstoreService],
   bootstrap: [AppComponent]
